1bd1d7779c228dd4e5f5738b59cbf9c616733e06
ranisaaulia <ranisaaulia31@gmail.com>
2023-05-18 00:08:29 +0700

    Day 2

--- Day1/Tugas Day 2.sql
+++
@@ -1,9 +0,0 @@
-create extension dblink;
-
-create foreign data wrapper postgres;
-
-create server localhost foreign data wrapper postgres options(hostaddr '127.0.0.1', dbname 'NorthwindDB');
-
-create user mapping for postgres server localhost options (user 'postgres', password 'admin');
-
-select dblink_connect ('localhost')
