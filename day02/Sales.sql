ab80cfa604667fd4b8ef5fe31e9a1aff79a6180b
wahyult98 <wahyult98@gmail.com>
2023-05-18 00:26:53 +0700

    D02

---
+++ day02/Sales.sql
@@ -0,0 +1,135 @@
+CREATE SCHEMA SALES
+
+CREATE EXTENSION DBLINK
+
+create foreign data wrapper postgres;
+create server localhost foreign data wrapper postgres options(hostaddr '127.0.0.1', dbname'NorthwindDB');
+create user mapping for postgres server localhost options (user 'postgres',password 'admin');
+
+select dblink_connect ('localhost')
+
+create table sales.shippers(
+	ship_id serial primary key,
+	ship_name varchar(40),
+	ship_phone varchar(24)
+)
+
+select * from dblink('localhost','select shipper_id,company_name,phone from shippers')
+as data(ship_id integer,ship_name varchar (40),ship_phone varchar(24))
+
+create table sales.customers (
+	cust_id varchar(10),
+	cust_name varchar(40),
+	cust_city varchar(15)
+)
+
+drop table sales.customers cascade 
+
+select * from dblink('localhost','select customer_id,company_name,city from customers')
+as data(cust_id varchar(10),cust_name varchar (40), cust_city varchar(15))
+
+
+create table sales.suppliers (
+	supr_id serial primary key,
+	supr_name varchar(40),
+	supr_contact_name varchar(30),
+	supr_city varchar(15)
+)
+
+
+select * from dblink('localhost','select supplier_id,company_name,contact_name,city from supplier')
+as data(supr_id integer,supr_name varchar (40), supr_contact_name varchar(30), supr_city varchar(15))
+
+
+create table sales.products (
+	prod_id serial primary key,
+	prod_name varchar(40),
+	prod_quantity varchar(20),
+	prod_price money,
+	prod_in_stock smallint,
+	prod_on_order smallint,
+	prod_reorder_level smallint,
+	prod_discontinued bit,
+	prod_cate_id int,
+	prod_surp_id int,
+	foreign key (prod_cate_id) references sales.categories(cate_id),
+	foreign key (prod_surp_id) references sales.suppliers(supr_id)
+)
+
+
+select * from dblink('localhost','select product_id,product_name,quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued from products')
+as data(prod_id integer, prod_name varchar(40), prod_quantity varchar(20), prod_price money, prod_in_stock smallint,
+	prod_in_order smallint,
+	prod_reorder_level smallint,
+	prod_discontinued bit)
+
+
+create table sales.orders(
+	order_id serial primary key,
+	order_date timestamp,
+	order_required_date timestamp,
+	order_shipped_date timestamp,
+	order_freight money,
+	order_subtotal money,
+	order_total_qty smallint,
+	order_ship_city varchar(15),
+	order_ship_address varchar(60),
+	order_status varchar(15),
+	order_cust_id integer,
+	order_ship_id integer,
+	foreign key (order_cust_id) references sales.customers(cust_id),
+	foreign key (order_ship_id) references sales.shippers(ship_id)
+)
+
+
+select * from dblink('localhost','select order_id,order_date,required_date, shipped_date, freight, ship_city, ship_address from orders')
+as data(order_id integer, order_date timestamp,
+	order_required_date timestamp,
+	order_shipped_date timestamp,
+	order_freight money,
+	order_ship_city varchar(15),
+	order_ship_address varchar(60)
+	)
+
+
+create table sales.categories (
+	cate_id serial primary key,
+	cate_name varchar(15),
+	cate_description text
+)
+
+
+select * from dblink('localhost','select category_id,category_name,description from categories')
+as data(cate_id integer,cate_name varchar (15), cate_description text)
+
+
+create table sales.orders_detail (
+	ordet_order_id integer,
+	ordet_prod_id integer,
+	ordet_price money,
+	ordet_quantity smallint,
+	ordet_discount real,
+	constraint sales_ordet_pk primary key (ordet_order_id, ordet_prod_id),
+	foreign key (ordet_order_id) references sales.orders(order_id),
+	foreign key(ordet_prod_id) references sales.products(prod_id)
+)
+
+
+select * from dblink('localhost','select order_id,unit_price,quantity, discount from order_detail')
+as data(ordet_order_id integer,
+	ordet_price money,
+	ordet_quantity smallint,
+	ordet_discount real
+	)
+
+
+
+
+
+
+
+
+
+
+
+
